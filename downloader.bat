1b1318a08a4947858e5c5556a686501b7df6a4cf
SergeyT <0tx0rx@gmail.com>
2025-02-02 02:16:19 +0700

    update ytdlp file format set output dir in file exit on output file missing replace hardcode with variables

--- downloader.bat
+++ downloader.bat
@@ -4,22 +4,41 @@ title DASH DL by BlazeR404
 
 set "yt_dlp=bin\yt-dlp.exe"
 set "links_file=links.txt"
-set "output_dir=output"
+set "output_file=output.txt"
+set /p output_dir=<%output_file%
 
 if not exist "%yt_dlp%" (
     echo Ошибка: yt-dlp.exe не найден!
-    pause
+    pause>nul
     exit /b 1
 )
 
 if not exist "%links_file%" (
-    echo Ошибка: links.txt не найден!
-    pause
+    echo Ошибка: %links_file% не найден!
+    pause>nul
+    exit /b 1
+)
+
+if not exist "%output_file%" (
+    echo Файл %output_file% не найден!
+    pause>nul
+    exit /b 1
+)
+
+
+if not defined output_dir (
+    echo Ошибка: Файл %output_file% пуст.
+    pause>nul
     exit /b 1
 )
 
 if not exist "%output_dir%" ( 
-	mkdir "%output_dir%"
+    mkdir "%output_dir%"
+    if errorlevel 1 (
+        echo Ошибка: Не удалось создать директорию "%output_dir%"
+        pause>nul
+        exit /b 1
+    )
 )
 
 set "count=0"
@@ -29,8 +48,8 @@ for /f "usebackq delims=" %%i in ("%links_file%") do (
     set "line=!line: =!"
 
     if defined line if not "!line:~0,1!"=="#" (
-		set /a count+=1
-				
+        set /a count+=1
+
         if !count! lss 10 (
             set "output_filename=e00!count!"
         ) else if !count! lss 100 (
@@ -41,13 +60,13 @@ for /f "usebackq delims=" %%i in ("%links_file%") do (
 
         echo ==============
         echo Загружаем: !output_filename!
-        "%yt_dlp%" -f "bestvideo+bestaudio" "%%i" -o "%output_dir%\!output_filename!.mp4"
+        "%yt_dlp%" -f best/bestvideo+bestaudio "%%i" -o "%output_dir%\!output_filename!.mp4"
         echo.
     )
 )
 
 if !count!==0 (
-    echo В файле links.txt нет ссылок для загрузки.
+    echo В файле %links_file% нет ссылок для загрузки.
     pause>nul
     exit /b 1
 )
--- launcher.bat
+++ launcher.bat
@@ -8,12 +8,14 @@ echo ==============================
 echo Что делаем?
 echo 1. Запустить скачивание
 echo 2. Открыть файл со ссылками
+echo.
+echo 0. Указать директорию для загрузки
 echo ==============================
 set /p choice="Ваш выбор: "
 
 if "%choice%"=="1" goto run_downloader
 if "%choice%"=="2" goto open_links_file
-
+if "%choice%"=="0" goto open_output_file
 goto menu
 
 :run_downloader
@@ -23,5 +25,10 @@ goto menu
 
 :open_links_file
 set "links=links.txt"
+start "" "%links%"
+goto menu
+
+:open_output_file
+set "links=output.txt"
 start "" "%links%"
 goto menu
